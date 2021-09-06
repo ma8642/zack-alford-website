58b87890536d748c940609716d809a4680a92b39
Marley Alford <marleycalford@gmail.com>
2021-09-06 14:22:22 -0400

    WIP - Tried more stuff with gear page

--- src/components/Gear.js
+++ src/components/Gear.js
@@ -3,9 +3,9 @@ import gearData from "../assets/gear/gear";
 import bgImage from "../assets/gear/gear-bg.png";
 
 const Gear = () => {
-  // TODO figure out a better way to have the on-hover happen so i don't have the a-tag warning anymore
+  // TODO figure out how to align-items-center this page content!!  and how to add space between brand logos
   const gear = gearData.map((gear) => (
-    <div className="w-1/2">
+    <div>
       <a href={gear.href}>
         <img key={gear.id} src={gear.src} alt={gear.alt} maxWidth="5%" />
       </a>
@@ -13,7 +13,7 @@ const Gear = () => {
   ));
 
   return (
-    <div className="grid grid-cols-2 gap-10">
+    <div className="flex justify-between items-center">
       <div className="flex flex-col justify-center">
         <img
           src={bgImage}
@@ -21,7 +21,9 @@ const Gear = () => {
           maxWidth="30%"
         />
       </div>
-      <div className="flex flex-col justify-around items-center">{gear}</div>
+      <div className="flex flex-col justify-evenly items-center h-full w-1/2">
+        {gear}
+      </div>
     </div>
   );
 };
