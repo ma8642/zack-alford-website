cd17ab0c6e268647a64d60ef20e7c20ee1b4305b
Marley Alford <marleycalford@gmail.com>
2022-06-14 01:05:23 -0400

    Added title to gallery collections and reformatted data

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import GalleryCollection from "./GalleryCollection";
 import galleryData from "../assets/gallery/galleries";
-import collectionsData from "../assets/gallery/gallery-collections";
 
 const Gallery = () => {
   // TODO figure out a better way to have the on-hover happen so i don't have the a-tag warning anymore
   const [openCollection, setOpenCollection] = useState(null);
   const galleries = galleryData.map((gallery) => (
-    <div
-      className="relative"
-      onClick={() => setOpenCollection(collectionsData[gallery.id])}
-    >
+    <div className="relative" onClick={() => setOpenCollection(gallery)}>
       <div className="absolute inset-0 z-10 bg-green-300 text-center text-black flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300 cursor-pointer">
         <h1 className="tracking-wider">{gallery.title}</h1>
         <p className="mx-auto">{gallery.year}</p>
@@ -31,8 +27,8 @@ const Gallery = () => {
   if (openCollection) {
     return (
       <GalleryCollection
-        photos={openCollection}
-        handleBackClick={() => setOpenCollection(false)}
+        gallery={openCollection}
+        handleCloseClick={() => setOpenCollection(null)}
       />
     );
   }
--- src/components/GalleryCollection.js
+++ src/components/GalleryCollection.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
-import { ArrowLeftOutlined } from "@ant-design/icons";
+import { CloseOutlined } from "@ant-design/icons";
 // import galleryData from "../assets/gallery/galleries";
+import collectionsData from "../assets/gallery/gallery-collections";
 
-const GalleryCollection = ({ photos, handleBackClick }) => {
+const GalleryCollection = ({ gallery, handleCloseClick }) => {
   // One big photo
   // a row of small thumbnails underneath. Each one clicked becomes the big one shown.
-  const [focusedImage, setFocusedImage] = useState(photos[0]);
+  const collection = collectionsData[gallery.id];
+  const [focusedImage, setFocusedImage] = useState(collection[0]);
 
   return (
     <div className="collection-container">
-      <ArrowLeftOutlined className="mb-2 text-xl" onClick={handleBackClick} />
+      <div className="flex justify-between">
+        <div className="gallery-info flex font-body">
+          <h2 className="mr-2">{gallery.title.toUpperCase()}</h2>{" "}
+          <h3>{gallery.year}</h3>
+        </div>
+        <CloseOutlined
+          className="flex justify-end mb-2 text-xl"
+          onClick={handleCloseClick}
+        />
+      </div>
       <div className="flex flex-col justify-center content-center">
         <div className="focused-image flex justify-center">
           <img src={focusedImage.src} height="100vh" alt={focusedImage.alt} />
         </div>
 
         <div className="thumbnail-row grid gap-1 grid-cols-6 grid-rows-1 overflow-x-auto">
-          {photos.map((photo, i) => {
+          {collection.map((photo, i) => {
             return (
               <div
                 key={i}
