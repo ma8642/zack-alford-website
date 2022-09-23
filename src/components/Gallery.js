29f8519126641121bc520b76e6b17ae9f0f92368
Marley Alford <marleycalford@gmail.com>
2022-09-23 15:52:05 -0400

    WIP debugging with some hardcoding

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -33,7 +33,7 @@ const Gallery = () => {
         <div className="h-48 flex flex-wrap content-center">
           <img
             key={gallery.id}
-            src={`.${gallery.src}`}
+            src={"./images/galleries/bruce.png"}
             alt={`${gallery.title} gallery`}
             maxWidth="10%"
           />
