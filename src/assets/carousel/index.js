ceeb43dc826aefafcebb4cff020b2626710aa689
Marley Alford <marleycalford@gmail.com>
2022-12-16 13:20:46 -0500

    imported carousel images

---
+++ src/assets/carousel/index.js
@@ -0,0 +1,22 @@
+import carousel1 from "../img/carousel/bg-1.jpg";
+import carousel2 from "../img/carousel/bg-2.jpg";
+
+const carouselData = [
+  {
+    id: 1,
+    alt: "Zack playing drums. The shot points up from a low angle through drum kit, framing Zach between cymbals and bass drum.",
+    src: carousel1,
+  },
+  {
+    id: 2,
+    alt: "Black and white action shot of Zack smiling while he plays drums.",
+    src: carousel2,
+  },
+  //   {
+  //     id: 3,
+  //     alt: "Red-tinted photo of Zack playing drums.",
+  //     src: "/images/the-next-day.png",
+  //   },
+];
+
+export { carouselData };
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Element } from "react-scroll";
-import carouselData from "../assets/carousel/carousel.json";
+import { carouselData } from "../assets/carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 
