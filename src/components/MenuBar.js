287f9ce88349bdc7b3a1e86948fa931d617ccdfc
Marley Alford <marleycalford@gmail.com>
2022-12-26 22:09:56 -0500

    WIP - Got menu items to showwhen hamburger is open

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -29,7 +29,7 @@ const MenuBar = ({ pages }) => {
 
   const mobileMenuItems = pages.map((title) => {
     return (
-      <li>
+      <li className="text-green-400">
         <Link
           activeClass="active"
           to={title}
@@ -44,32 +44,63 @@ const MenuBar = ({ pages }) => {
     );
   });
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6 bg-black z-50 w-xl">
+    <nav className="flex items-center justify-between flex-wrap p-6 bg-black w-xl">
       <div className="w-full block">
         <div className="text-sm flex flex-row justify-end items-center font-body">
           <div
             className="MOBILE-MENU flex flex-col lg:hidden"
             onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
           >
-            {isMobileMenuOpen ? (
+            <>
+              <span className="w-8 bg-green-400 px-4 py-0.5 mb-2 mr-4"> </span>
+              <span className="w-8 bg-green-400 px-4 py-0.5 mb-2 mr-4"> </span>
+              <span className="w-8 bg-green-400 px-4 py-0.5 mb-2 mr-4"> </span>
+            </>
+            <div className={isMobileMenuOpen ? "showMenuNav" : "hideMenuNav"}>
+              <div
+                className="absolute top-0 right-0 px-8 py-8"
+                onClick={() => setIsMobileMenuOpen(false)}
+              >
+                <svg
+                  className="h-8 w-8 text-green-400"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
               <ul>{mobileMenuItems}</ul>
-            ) : (
-              <>
-                <span className="w-8 bg-green-400 px-4 py-0.5 mb-2 mr-4">
-                  {" "}
-                </span>
-                <span className="w-8 bg-green-400 px-4 py-0.5 mb-2 mr-4">
-                  {" "}
-                </span>
-                <span className="w-8 bg-green-400 px-4 py-0.5 mb-2 mr-4">
-                  {" "}
-                </span>
-              </>
-            )}
+            </div>
           </div>
           <div className="DESKTOP-MENU hidden lg:flex">{desktopMenuItems}</div>
         </div>
       </div>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        font-weight: bolder;
+        line-height: 2.5;
+      }
+    `}</style>
     </nav>
   );
 };
